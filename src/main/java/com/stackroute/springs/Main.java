915f3bce66b4744e7833d04e919444d36ff6a9a9
smashour <sherin.mashour@gmail.com>
2019-05-14 10:08:53 +0530

    moidified program

--- src/main/java/com/stackroute/springs/Main.java
+++ src/main/java/com/stackroute/springs/Main.java
@@ -1,6 +1,6 @@
 package com.stackroute.springs;
 
-import com.stackroute.springs.bean.Actor;
+
 import com.stackroute.springs.bean.Movie;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -9,7 +9,6 @@ public class Main {
     public static void main(String[] args) {
         ApplicationContext context=new ClassPathXmlApplicationContext("beans.xml");
         Movie movie=(Movie) context.getBean("movie");
-        //Actor actor=(Actor) context.getBean("actor");
         System.out.println(movie.getActor());
     }
 }
